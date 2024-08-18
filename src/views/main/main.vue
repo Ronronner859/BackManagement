88c6f054c37ccdeb2386b5b2349ef08f15e67fbf
chenyi <chenyilong916002@gmail.com>
2024-08-18 22:18:15 +0800

    feat:配置自定义文件

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class=""></div>
+  <div class="login">
+    <h1>login</h1>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.login {
+}
+</style>
