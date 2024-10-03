f25198283a0be026f1b3ef23ee4bd5c7fca0da87
chenyi <chenyilong916002@gmail.com>
2024-10-04 00:03:38 +0800

    feat:注册icons

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,31 @@ import pinia from './store'
 import './service/modules/home'
 import './assets/tailwind/output.css' // 引入tailwindcss
 import { createI18n } from 'vue-i18n'
+import registerIcons from './global/register-icons'
+const app = createApp(App)
 // 1、创建中文语言包对象
 const zh = {
   account: '账号登录',
-  phone: '手机登录'
+  phone: '手机登录',
+  username: '用户名',
+  password: '密码',
+  scan: '扫码登录',
+  remember: '记住我',
+  forget: '忘记密码',
+  login: '登录',
+  BackendManagementSystem: '后台管理系统'
 }
 // 2、创建英文语言包对象
 const en = {
   account: 'Account',
-  phone: 'Phone'
+  phone: 'Phone',
+  username: 'Username',
+  password: 'Password',
+  scan: 'Scan',
+  remember: 'Remember me',
+  forget: 'Forget password',
+  login: 'Login',
+  BackendManagementSystem: 'Backend Management System'
 }
 // 3、组合语言包对象
 const messages = {
@@ -26,4 +42,4 @@ const i18n = createI18n({
   locale: 'en',
   messages
 })
-createApp(App).use(router).use(pinia).use(i18n).mount('#app')
+createApp(App).use(router).use(pinia).use(i18n).use(registerIcons).mount('#app')
--- src/views/login/child-cpns/login-panel.vue
+++ src/views/login/child-cpns/login-panel.vue
@@ -1,19 +1,62 @@
 <template>
   <div class="login-panel">
-    <h1 class="login-title text-2xl font-bold">Beaky CMS (后台管理系统)</h1>
+    <h1 class="login-title text-2xl font-bold">
+      Beaky CMS ({{ $t('BackendManagementSystem') }})
+    </h1>
     <div class="login-tabs">
       <el-tabs
         type="border-card"
         v-model="activeName"
         @tab-click="handleClick"
         class="login-tabs"
+        stretch
       >
-        <el-tab-pane :label="$t('account')" name="account"> </el-tab-pane>
-        <el-tab-pane :label="$t('phone')" name="phone"> </el-tab-pane>
+        <el-tab-pane :label="$t('account')" name="account">
+          <el-form
+            :model="form"
+            :rules="rules"
+            ref="formRef"
+            label-width="90px"
+          >
+            <el-form-item prop="username" :label="$t('username')">
+              <el-input v-model="form.username" :placeholder="$t('username')" />
+            </el-form-item>
+            <el-form-item prop="password" :label="$t('password')">
+              <el-input v-model="form.password" :placeholder="$t('password')" />
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+        <el-tab-pane :label="$t('phone')" name="phone">
+          <el-form
+            :model="form"
+            :rules="rules"
+            ref="formRef"
+            label-width="86px"
+          >
+            <el-form-item prop="phone" :label="$t('phone')">
+              <el-input v-model="form.phone" :placeholder="$t('phone')" />
+            </el-form-item>
+            <el-form-item prop="password" :label="$t('password')">
+              <el-input v-model="form.password" :placeholder="$t('password')" />
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+        <!-- 扫码登录  TODO:后续集成-->
+        <el-tab-pane :label="$t('scan')" name="scan">
+          <div class="login-scan">
+            <img src="@/assets/img/Beaky-CMS.png" alt="" />
+          </div>
+        </el-tab-pane>
       </el-tabs>
     </div>
     <div class="login-remember">
-      <el-checkbox v-model="rememberMe">记住我</el-checkbox>
+      <el-checkbox v-model="rememberMe">{{ $t('remember') }}</el-checkbox>
+      <el-link type="primary">{{ $t('forget') }}</el-link>
+    </div>
+    <div class="login-submit">
+      <el-button type="primary" class="w-full mt-2 !h-10 !text-lg">{{
+        $t('login')
+      }}</el-button>
     </div>
   </div>
 </template>
@@ -23,6 +66,26 @@ import { ref } from 'vue'
 
 const activeName = ref<'account' | 'phone'>('account')
 const rememberMe = ref<boolean>(false)
+
+const form = ref<{
+  username: string
+  password: string
+  phone: string
+}>({
+  username: '',
+  password: '',
+  phone: ''
+})
+
+const rules = ref<{
+  username: { required: boolean; message: string }[]
+  password: { required: boolean; message: string }[]
+  phone: { required: boolean; message: string }[]
+}>({
+  username: [{ required: true, message: '请输入用户名' }],
+  password: [{ required: true, message: '请输入密码' }],
+  phone: [{ required: true, message: '请输入手机号' }]
+})
 const handleClick = (tab: 'account' | 'phone') => {
   activeName.value = tab
 }
@@ -30,8 +93,8 @@ const handleClick = (tab: 'account' | 'phone') => {
 
 <style lang="scss" scoped>
 .login-panel {
-  width: 320px;
-  height: 320px;
+  width: 420px;
+  height: 420px;
   background-color: #ffffff;
   border-radius: 10px;
   padding: 20px;
@@ -41,6 +104,28 @@ const handleClick = (tab: 'account' | 'phone') => {
   font-size: 20px;
 }
 .login-tabs {
-  // margin-top: 20px;
+  margin-top: 20px;
+}
+.login-remember {
+  display: flex;
+  justify-content: space-between;
+}
+.login-submit {
+  margin-top: 40px;
+  text-align: center;
+  .el-button {
+    width: 60%;
+    height: 40px;
+    font-size: 16px;
+  }
+}
+.login-scan {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img {
+    width: 30%;
+    height: 30%;
+  }
 }
 </style>
